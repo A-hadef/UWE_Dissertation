76a37edc90007d8400ff8aee569f1b6370763489
benedictelsom <benedict.elsom@gmail.com>
2020-10-11 12:03:57 +0100

    spring initialiser app

---
+++ src/main/java/com/uwe/dissertation/ins/UweDissertationApplication.java
@@ -0,0 +1,13 @@
+package com.uwe.dissertation.ins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UweDissertationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UweDissertationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uwedissertation/UweDissertationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uwedissertation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UweDissertationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
